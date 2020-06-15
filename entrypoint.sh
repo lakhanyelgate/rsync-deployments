92f0477162a9a6c33727dacf287e043a8c6b4882
lakhanyelgate <66018388+lakhanyelgate@users.noreply.github.com>
2020-06-15 20:33:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Start the SSH agent and load key.
 source agent-start "$GITHUB_ACTION"
 echo "$INPUT_REMOTE_KEY" | agent-add
-echo $
+echo $INPUT_REMOTE_KEY
 # Add strict errors and deploy.
 set -x
 sh -c "rsync $INPUT_SWITCHES -e 'ssh -o StrictHostKeyChecking=no -p $INPUT_REMOTE_PORT $INPUT_RSH' $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST:$INPUT_REMOTE_PATH"
